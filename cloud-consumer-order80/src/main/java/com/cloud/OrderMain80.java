9f5b9a716064c779e354b02b02359ce07b0f290a
ytlxy <a739771409@gmail.com>
2020-12-18 17:23:04 +0800

    new

--- cloud-consumer-order80/src/main/java/com/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/cloud/OrderMain80.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class PaymentMain8001 {
-    public static void main(String[] args){
-        SpringApplication.run(PaymentMain8001.class,args);
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/cloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cloud/controller/PaymentController.java
@@ -4,21 +4,19 @@ import com.cloud.entities.CommonResult;
 import com.cloud.entities.Payment;
 import com.cloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @Slf4j
 public class PaymentController {
+
     @Resource
     private PaymentService paymentService;
 
-    @PostMapping("/payment/create")
-    public CommonResult save(Payment payment){
+    @PostMapping("/payment/save")
+    public CommonResult<Payment> save(@RequestBody Payment payment){
         int result = paymentService.save(payment);
         log.info("*****插入结果:"+result);
         if (result > 0){
@@ -29,7 +27,7 @@ public class PaymentController {
     }
 
     @GetMapping("/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果:"+payment);
         if (payment != null){
