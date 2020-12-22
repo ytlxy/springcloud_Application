38dac2daea72a9d449d3d53c57ef8f49a88c97d7
ytlxy <a739771409@gmail.com>
2020-12-22 12:08:51 +0800

    new

--- cloud-provider-payment8001/src/main/java/com/cloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -59,8 +60,16 @@ public class PaymentController {
         return this.discoveryClient;
     }
 
-    @GetMapping(value = "/payment/lb")
+    @GetMapping("/payment/lb")
     public String getPaymentLB() {
+        return serverPort;
+    }
+
+    @GetMapping("/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e){e.printStackTrace();}
         return serverPort;
     }
 }
--- cloud-provider-payment8002/src/main/java/com/cloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/cloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -61,6 +62,14 @@ public class PaymentController {
 
     @GetMapping(value = "/payment/lb")
     public String getPaymentLB() {
+        return serverPort;
+    }
+
+    @GetMapping("/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e){e.printStackTrace();}
         return serverPort;
     }
 }
