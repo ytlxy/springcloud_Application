7e7cd77ef918093ea93ff380fd8d3a1cc19bf13a
ytlxy <a739771409@gmail.com>
2020-12-29 15:28:34 +0800

    new

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/cloud/controller/ControllerClientController.java
@@ -0,0 +1,19 @@
+package com.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ControllerClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
