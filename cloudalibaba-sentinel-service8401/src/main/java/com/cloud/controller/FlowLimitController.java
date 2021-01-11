5edf51ac41f5e79bd01e74e0b0997954b1289959
ytlxy <a739771409@gmail.com>
2021-01-11 18:40:21 +0800

    new

--- cloudalibaba-sentinel-service8401/src/main/java/com/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cloud/controller/FlowLimitController.java
@@ -1,11 +1,13 @@
 package com.cloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -21,5 +23,16 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB(){
         return "testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+      log.info("testD 测试RT");
+        return "-----testD";
     }
 }
