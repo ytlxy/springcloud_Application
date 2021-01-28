6ae24aab273553392cc322772c7d95df5ac251c7
ytlxy <a739771409@gmail.com>
2021-01-28 16:51:57 +0800

    new

---
+++ seata-storage-service2002/src/main/java/com/cloud/controller/StorageController.java
@@ -0,0 +1,20 @@
+package com.cloud.controller;
+
+import com.cloud.domain.CommonResult;
+import com.cloud.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StorageController {
+
+    @Autowired
+    private StorageService storageService;
+
+    @GetMapping("/storage/decrease")
+    public CommonResult decrease(Long productId,Integer count){
+        storageService.decrease(productId,count);
+        return new CommonResult(200,"扣减库存成功!");
+    }
+}
