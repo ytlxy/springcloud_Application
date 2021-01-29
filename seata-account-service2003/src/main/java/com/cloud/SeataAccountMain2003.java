bd4dfd1646465f31df68fb34a7d7207b36c374f4
ytlxy <a739771409@gmail.com>
2021-01-29 18:41:08 +0800

    new

---
+++ seata-account-service2003/src/main/java/com/cloud/SeataAccountMain2003.java
@@ -0,0 +1,15 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SeataAccountMain2003 {
+    public static void main(String[] args) {
+        SpringApplication.run(SeataAccountMain2003.class,args);
+    }
+}
